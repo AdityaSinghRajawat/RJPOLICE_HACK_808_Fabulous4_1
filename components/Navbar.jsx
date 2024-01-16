cade0b2a6d04438ff4e97e9d0013cc25be85b519
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2024-01-16 13:55:40 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     return (
         <nav className={`sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
 
-            {pathname != "/" && pathname != "/login" &&
+            {pathname != "/" && pathname != "/login" && pathname != "/createuser" &&
                 <div className='mr-3'>
                     <MobileSidebar />
                 </div>
